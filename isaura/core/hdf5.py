68961c2284cda4b8c641b1eac9c3eb85224ac180
JHlozek <jason.hlozek@uct.ac.za>
2021-10-06 17:38:59 +0200

    Hdf5Explorer inherits from Hdf5 class

--- isaura/core/hdf5.py
+++ isaura/core/hdf5.py
@@ -1,31 +1,31 @@
-from .base import IsauraBase
+from isaura.core.base import IsauraBase
+from isaura.handle.io import Hdf5
 import h5py
 import os
+import numpy as np
 
 
-class Hdf5Explorer(IsauraBase):
+class Hdf5Explorer(Hdf5):
 
     def __init__(self, model_id):
-        IsauraBase.__init__(self, model_id=model_id)
+        Hdf5.__init__(self, model_id=model_id)
 
     def exists(self):
         return self._check_h5_exists()
 
     def apis(self):
-        with h5py.File(self.data_path, "r") as f:
-            api_names = [k for k in f.keys()]
-            return api_names
+        return self.list_apis()
             
     @property
     def size(self):
         return os.path.getsize(self.data_path)
 
 
-class Hdf5ApiExplorer(IsauraBase):
+class Hdf5ApiExplorer(Hdf5):
 
     def __init__(self, model_id, api_name):
-        IsauraBase.__init__(self, model_id=model_id)
-        self.api_name = api_name
+        Hdf5.__init__(self, model_id=model_id)
+        self.set_curr_api(api_name)
 
     def exists(self):
         return self._check_api_exists(self.api_name)
@@ -39,3 +39,10 @@ class Hdf5ApiExplorer(IsauraBase):
     def dtype(self):
         with h5py.File(self.data_path, "rb") as f:
             return f[self.api_name][data].dtype
+
+if __name__ == "__main__":  #TESTING
+    h = Hdf5Explorer("eos4e40")
+    h.set_curr_api("Predict")
+    h.write_api(["y","z"], [[90, np.nan, 70], [61, 51, 41]])
+    for line in h.read_api():
+        print(line)
--- isaura/handle/io.py
+++ isaura/handle/io.py
@@ -1,9 +1,8 @@
-import numpy as np
 from isaura.core.base import IsauraBase
 from isaura.handle.reader import Reader
 from isaura.handle.writer import Writer
 from isaura.handle.mapper import Mapper
-
+import numpy as np
 
 class Hdf5(IsauraBase):
 
@@ -12,44 +11,44 @@ class Hdf5(IsauraBase):
         self.r = Reader(self.model_id)
         self.w = Writer(self.model_id)
         self.m = Mapper(self.model_id)
+        self.api_name = ""
 
-    def read_api(self, api_name):
-        return self.r.yield_api(api_name)
+    def set_curr_api(self, api):
+        self.api_name = api
+
+    def get_curr_api(self):
+        return self.api_name
+
+    def read_api(self):
+        return self.r.yield_api(self.api_name)
 
     def list_apis(self):
         return self.r.get_apis()
 
-    def list_keys(self, api_name):
-        return self.r._get_keys(api_name)
+    def list_keys(self):
+        return self.r._get_keys(self.api_name)
 
-    def read_by_key(self, api_name, key_list):
-        return self.r.read_by_key(api_name, key_list)
+    def read_by_key(self, key_list):
+        return self.r.read_by_key(self.api_name, key_list)
 
-    def read_by_index(self, api_name, index_list):
-        return self.r.read_by_idx(api_name, index_list)
+    def read_by_index(self, index_list):
+        return self.r.read_by_idx(api_name, self.index_list)
 
-    def check_exists(self, api_name, key_list):
+    def check_keys_exist(self, api_name, key_list):
         return self.m.check_keys(api_name, key_list)
 
-    def filter_keys(self, api_name, keys, values):
+    def filter_keys(self, keys, values):
         arr_keys = np.array(keys)
         arr_values = np.array(list(values))
-        return self.w._filter_keys(api_name, keys, values)
+        return self.w._filter_keys(self.api_name, keys, values)
 
-    def write_api(self, api_name, keys, values):
-        self.w.write(api_name, keys, values)
+    def write_api(self, keys, values):
+        self.w.write(self.api_name, keys, values)
 
-    def merge(self, old_model, old_api_name, curr_api):
+    def merge(self, old_model, old_api_name):
         reader = Reader(old_model)
         for key, value in reader.yield_api(old_api_name):   #Inefficient
-            self.w.write(curr_api, key, value)
+            self.w.write(self.api_name, key, value)
 
     def _resolve_dtype_clash(self, curr_h5, new_data):
         pass
-
-if __name__ == "__main__":  #TESTING
-    h = Hdf5("eos4e40")
-    h.write_api("Predict", ["y","z"], [[90, np.nan, 70], [61, 51, 41]])
-    h.merge("eos4e40_1", "Predict", "Predict")
-    for line in h.read_api("Predict"):
-        print(line)
