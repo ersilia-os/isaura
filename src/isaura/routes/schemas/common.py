b9df948649aac51006ea3f4fb4d78f1c9be6bc66
Ankur Kumar <ank@leoank.me>
2022-11-21 14:04:26 +0530

    update preacalc schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/isaura/routes/schemas/common.py
+++ src/isaura/routes/schemas/common.py
@@ -1,9 +1,33 @@
 """Common schemas."""
 
-from pydantic import BaseModel
+from decimal import Decimal
+import json
+from typing import Dict
+
+from pydantic import BaseModel, Field, validator
+
+from isaura.utils.aws import decimal_json_decoder
 
 
 class RequestContext(BaseModel):
     """Common request context model."""
 
     pass
+
+
+class Precalc(BaseModel):
+    """Schema for Preclac object."""
+
+    model_id: str = Field(alias="pk")
+    input_hash: str = Field(alias="sk")
+    value: Dict
+
+    @validator("value")
+    def convert_float_to_decimals(cls, v):
+        return json.loads(
+            json.dumps(v, default=decimal_json_decoder), parse_float=Decimal
+        )
+
+    class Config:
+        ignore_extra = False
+        allow_population_by_field_name = True
